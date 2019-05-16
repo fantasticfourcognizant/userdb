c87b082c51e30cc3189f0b2e29320de0030abad4
mdx919 <thanvirdx919@gmail.com>
2019-05-16 16:44:54 -0400

    login function implemented

--- src/main/java/com/cognizant/user/controller/UserController.java
+++ src/main/java/com/cognizant/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.cognizant.user.controller;
 
+import com.cognizant.user.model.Login;
 import com.cognizant.user.model.User;
 import com.cognizant.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +24,36 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/login")
+    public ResponseEntity validateUser(@RequestBody Login login) {
+        ObjectMapper mapper = new ObjectMapper();
+        User user = userService.validateUser(login);
+
+        if(user == null) {
+            return new ResponseEntity<>("User doesn't Exist!", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+
+        String json = new String();
+        try{
+            json = mapper.writeValueAsString(user);
+        }catch (Exception  e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity(json, HttpStatus.OK);
+    }
+
     @PostMapping("/signup")
     public ResponseEntity<String> createNewUser(@RequestBody User user) {
 
         User newUser = userService.createUser(user);
 
         if(newUser == null) {
-            return new ResponseEntity<>("User Already Exists!", HttpStatus.UNPROCESSABLE_ENTITY);
+            return new ResponseEntity<>("User Already Exist!", HttpStatus.UNPROCESSABLE_ENTITY);
         }
-
         return new ResponseEntity<>("User Created!", HttpStatus.CREATED);
     }
+
+
 }
--- src/main/java/com/cognizant/user/service/UserService.java
+++ src/main/java/com/cognizant/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.cognizant.user.service;
 
+import com.cognizant.user.model.Login;
 import com.cognizant.user.model.User;
 import com.cognizant.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,15 @@ public class UserService {
         }
         userRepository.save(user);
         return user;
+    }
+
+    public User validateUser(Login login) {
+        List<User> users = userRepository.findAll();
+
+        User user = users.stream()
+                .filter(u -> u.getEmail().equals(login.getEmail()) && u.getPassword().equals(login.getPassword()))
+                .findFirst().orElse(null);
+        if(user == null) return null;
+        else return user;
     }
 }
