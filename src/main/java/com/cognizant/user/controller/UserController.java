0d8fb55eb4ab16735c9d1f0b789f94832f747e28
mdx919 <thanvirdx919@gmail.com>
2019-05-17 15:49:44 -0400

    added User to api gateway

--- src/main/java/com/cognizant/user/controller/UserController.java
+++ src/main/java/com/cognizant/user/controller/UserController.java
@@ -5,16 +5,13 @@ import com.cognizant.user.model.User;
 import com.cognizant.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     private UserService userService;
@@ -55,5 +52,9 @@ public class UserController {
         return new ResponseEntity<>("User Created!", HttpStatus.CREATED);
     }
 
+    @GetMapping("/")
+    public String useerService(){
+        return "user service";
+    }
 
 }
